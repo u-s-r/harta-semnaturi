35d24db66681b45c5ce6a1e93183b61b9840b66b
Mihai Chereji <mihai@cronco.me>
2016-09-20 21:52:35 +0300

    Merge conflicts, add chroma and add individual color scales to each county.

--- assets/harta.js
+++ assets/harta.js
@@ -25,29 +25,27 @@
 		};
 
 		info.addTo(map);
-
+		var scale = chroma.scale(['#ffffff','#0084ff']).domain([0, 10000]);
 		// get color depending on population density value
 		function getColor(prop) {
-			//FIXME: this shows 10 times the actual intensity, s/1000/100/ later
-			var d = prop.target == 0 ? 0 : Math.floor(prop.signatures*1000/prop.target);
-			return d >	90	? '#800026' :
-			       d >	80	? '#bd0026' :
-			       d >	70	? '#e31a1c' :
-			       d >	60	? '#fc4e2a' :
-			       d >	50      ? '#fd8d3c' :
-			       d >	40	? '#feb24c' :
-			       d >	30	? '#fed976' :
-			       d >	20	? '#ffeda0' :
-			                  '#ffffcc';
+			var color;
+			console.log(prop);
+			if (prop.scale) {
+				color = prop.scale(prop.signatures).toString();
+			} else {
+				color = scale(prop.signatures).toString();
+			}
+			return color;
 		}
 
 		function style(feature) {
 			return {
 				weight: 1,
 				opacity: 1,
-				color: 'white',
-				fillOpacity: 0.7,
-				fillColor: getColor(feature.properties)
+				color: '#0084ff',
+				fillOpacity: 1,
+				fillColor: getColor(feature.properties),
+				strokeColor: '#0084ff'
 			};
 		}
 
@@ -55,7 +53,7 @@
 			var layer = e.target;
 
 			layer.setStyle({
-				weight: 2,
+				weight: 4,
 				color: '#666',
 				dashArray: '3',
 				fillOpacity: 0.7
@@ -89,6 +87,8 @@
 		  if (f != null ) {
 		      geoInfo.features[i].properties.signatures = f[0]
 		      geoInfo.features[i].properties.target = f[1]
+			  console.log(geoInfo.features[i].id, f[1]);
+			  geoInfo.features[i].properties.scale = chroma.scale(['#ffffff','#0084ff']).domain([0, f[1]]);
 	          }
 		}
 
